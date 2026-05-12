ace0f01d6f3e4da3f62c16216b79c6be2aeaa0de
BrandoonRH <aeroatlas.51@gmail.com>
2026-05-12 11:59:57 -0600

    bug Dynamic Rendering in build, add connection() from next server

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -3,16 +3,23 @@ import { generatePageTitle } from "@/src/shared/utils/Metadata";
 import { Metadata } from "next";
 import Link from "next/link";
 import SetPasswordForm from "@/src/features/auth/components/SetPasswordForm";
+import { Suspense } from "react";
+import { connection } from "next/server";
 
 export const metadata: Metadata = {
   title: generatePageTitle("Restablecer Password"),
 };
 
-export default function ForgotPasswordPage() {
+export default async function ForgotPasswordPage() {
+  await connection();
   return (
     <>
       <Heading>Restablece tu Contraseña</Heading>
       <SetPasswordForm />
+
+      {/*  <Suspense fallback={<p>Cargando...</p>}>
+        <SetPasswordForm />
+      </Suspense> */}
 
       <nav className="mt-20 flex justify-between">
         <Link href="/auth/register">Crear Cuenta</Link>
